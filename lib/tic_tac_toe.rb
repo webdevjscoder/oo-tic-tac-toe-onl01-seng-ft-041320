e776e55ed6c055c42a6f8fc200f78604070832c8
Juan Cobian <cobianjuan19@gmail.com>
2020-04-22 19:38:27 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,90 @@
+require 'pry'
+
+class TicTacToe
+  
+  WIN_COMBINATIONS =
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [6,4,2]
+  
+  def initialize(board = nil)
+    # this creates a new empty array with 9 empty strings.
+    @board = board || Array.new(9, " ")
+  end
+  
+  def display_board
+    # displays tictactoe board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    integer = user_input.to_i - 1
+  end
+  
+  def move(board_index, current_player)
+    @board[board_index] = current_player
+  end
+  
+  def position_taken?(location)
+    @board[location] != " " # && @board[location] != " "
+  end
+  
+  def valid_move?(board_index)
+    board_index.between?(0,8) && !position_taken?(board_index)
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.chomp
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+      binding.pry
+    else
+      turn
+    end
+  end
+  
+  def turn_count
+    
+  end
+  
+  def current_player
+    
+  end
+  
+  def won?
+    
+  end
+  
+  def full?
+    
+  end
+  
+  def draw?
+    
+  end
+  
+  def over?
+    
+  end
+  
+  def winner
+    
+  end
+  
+  def play
+    
+  end
+  
+end 
